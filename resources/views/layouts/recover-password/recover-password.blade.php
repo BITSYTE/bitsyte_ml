3f695234c73cf761845df52aa04e57be69ede29a
franz navarro <f2_@hotmail.com>
2017-08-04 13:30:36 -0500

    vistas en blade
    layouts/base recibe title para titulo de la pagina y contiene los estilos y scritps, todas las vistas la heredan
    
    layouts/userlayout recibe el @username contiene el footer y header para los menus del usuario
    
    layouts/dashboard hereda userslayout y contiene la vista por defecto de inicio
    
    evista error404  login register recoverpassword, heredan layouts/base

---
+++ resources/views/layouts/recover-password/recover-password.blade.php
@@ -0,0 +1,45 @@
+@extends('layout.base')
+
+@section('body')
+  <body data-open="click" data-menu="horizontal-menu" data-col="1-column" class="horizontal-layout horizontal-menu 1-column  blank-page blank-page">
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+    <div class="app-content container center-layout mt-2">
+      <div class="content-wrapper">
+        <div class="content-header row">
+        </div>
+        <div class="content-body"><section class="flexbox-container">
+    <div class="col-md-4 offset-md-4 col-xs-10 offset-xs-1 box-shadow-2 p-0">
+        <div class="card border-grey border-lighten-3 px-2 py-2 m-0">
+            <div class="card-header no-border pb-0">
+                <div class="card-title text-xs-center">
+                    <img src="../../app-assets/images/logo/stack-logo-dark.png" alt="branding logo">
+                </div>
+                <h6 class="card-subtitle line-on-side text-muted text-xs-center font-small-3 pt-2"><span>We will send you a link to reset password.</span></h6>
+            </div>
+            <div class="card-body collapse in">
+                <div class="card-block">
+                    <form class="form-horizontal" action="login-simple.html" novalidate>
+                        <fieldset class="form-group position-relative has-icon-left">
+                            <input type="email" class="form-control form-control-lg input-lg" id="user-email" placeholder="Your Email Address" required>
+                            <div class="form-control-position">
+                                <i class="ft-mail"></i>
+                            </div>
+                        </fieldset>
+                        <button type="submit" class="btn btn-outline-primary btn-lg btn-block"><i class="ft-unlock"></i> Recover Password</button>
+                    </form>
+                </div>
+            </div>
+            <div class="card-footer no-border">
+                <p class="float-sm-left text-xs-center"><a href="../login/login.html" class="card-link">Login</a></p>
+                <p class="float-sm-right text-xs-center">New to BitSite ? <a href="../register/register.html" class="card-link">Create Account</a></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+        </div>
+      </div>
+    </div>
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+
+@endsection
