0ad09cc2f389652b7d4beb2c94264abd60cd76f3
franz navarro <f2_@hotmail.com>
2017-08-09 14:17:40 -0500

    ltr

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -14,29 +14,29 @@
           <link rel="shortcut icon" type="image/x-icon" href="{{ asset('app-assets/images/ico/favicon.ico')}}">
           <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i%7COpen+Sans:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
           <!-- BEGIN VENDOR CSS-->
-          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css-rtl/bootstrap.css')}}">
-          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/fonts/feather/style.min.css')}}">
-          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/fonts/font-awesome/css/font-awesome.min.css')}}">
-          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/fonts/flag-icon-css/css/flag-icon.min.css')}}">
+          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap.css')}}">
+          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/fonts/feather/style.css')}}">
+          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/fonts/font-awesome/css/font-awesome.css')}}">
+          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/fonts/flag-icon-css/css/flag-icon.css')}}">
           <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/extensions/pace.css')}}">
           <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/charts/jquery-jvectormap-2.0.3.css')}}">
           <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/charts/morris.css')}}">
           <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/extensions/unslider.css')}}">
-          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/weather-icons/climacons.min.css')}}">
+          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/weather-icons/climacons.css')}}">
           <!-- END VENDOR CSS-->
           <!-- BEGIN STACK CSS-->
-          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css-rtl/bootstrap-extended.css')}}">
-          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css-rtl/app.css')}}">
-          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css-rtl/colors.css')}}">
-          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css-rtl/custom-rtl.css')}}">
+          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap-extended.css')}}">
+          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/app.css')}}">
+          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/colors.css')}}">
+          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/custom.css')}}">
           <!-- END STACK CSS-->
           <!-- BEGIN Page Level CSS-->
-          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css-rtl/core/menu/menu-types/horizontal-menu.css')}}">
-          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css-rtl/core/menu/menu-types/vertical-overlay-menu.css')}}">
+          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/core/menu/menu-types/horizontal-menu.css')}}">
+          <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/core/menu/menu-types/vertical-overlay-menu.css')}}">
 
           <!-- END Page Level CSS-->
           <!-- BEGIN Custom CSS-->
-          <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style-rtl.css')}}">
+          <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css')}}">
           <!-- END Custom CSS-->
     @show
   </head>
@@ -48,23 +48,23 @@
 
     @section('scripts')
         <!-- BEGIN VENDOR JS-->
-        <script src="{{ asset('app-assets/vendors/js/vendors.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('app-assets/vendors/js/vendors.js')}}" type="text/javascript"></script>
         <!-- BEGIN VENDOR JS-->
         <!-- BEGIN PAGE VENDOR JS-->
         <script type="text/javascript" src="{{ asset('app-assets/vendors/js/ui/jquery.sticky.js')}}"></script>
-        <script type="text/javascript" src="{{ asset('app-assets/vendors/js/charts/jquery.sparkline.min.js')}}"></script>
-        <script src="{{ asset('app-assets/vendors/js/extensions/jquery.knob.min.js" type="text/javascript')}}"></script>
+        <script type="text/javascript" src="{{ asset('app-assets/vendors/js/charts/jquery.sparkline.js')}}"></script>
+        <script src="{{ asset('app-assets/vendors/js/extensions/jquery.knob.js" type="text/javascript')}}"></script>
         <script src="{{ asset('app-assets/js/scripts/extensions/knob.js" type="text/javascript')}}"></script>
         <script src="{{ asset('app-assets/vendors/js/charts/raphael-min.js" type="text/javascript')}}"></script>
-        <script src="{{ asset('app-assets/vendors/js/charts/morris.min.js" type="text/javascript')}}"></script>
-        <script src="{{ asset('app-assets/vendors/js/charts/jvector/jquery-jvectormap-2.0.3.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('app-assets/vendors/js/charts/morris.js" type="text/javascript')}}"></script>
+        <script src="{{ asset('app-assets/vendors/js/charts/jvector/jquery-jvectormap-2.0.3.js')}}" type="text/javascript"></script>
         <script src="{{ asset('app-assets/vendors/js/charts/jvector/jquery-jvectormap-world-mill.js')}}" type="text/javascript"></script>
         <script src="{{ asset('app-assets/data/jvector/visitor-data.js')}}" type="text/javascript"></script>
-        <script src="{{ asset('app-assets/vendors/js/charts/chart.min.js')}}" type="text/javascript"></script>
-        <script src="{{ asset('app-assets/vendors/js/charts/jquery.sparkline.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('app-assets/vendors/js/charts/chart.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('app-assets/vendors/js/charts/jquery.sparkline.js')}}" type="text/javascript"></script>
         <script src="{{ asset('app-assets/vendors/js/extensions/unslider-min.js')}}" type="text/javascript"></script>
-        <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css-rtl/core/colors/palette-climacon.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/fonts/simple-line-icons/style.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/core/colors/palette-climacon.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/fonts/simple-line-icons/style.css')}}">
         <!-- END PAGE VENDOR JS-->
         <!-- BEGIN STACK JS-->
         <script src="{{ asset('app-assets/js/core/app-menu.js')}}" type="text/javascript"></script>
