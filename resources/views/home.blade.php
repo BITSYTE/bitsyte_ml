2a0ab9f93a3c7a84f303d75a8a4be5cf6af1b1a7
franz navarro <f2_@hotmail.com>
2017-08-18 12:58:50 -0500

    user name
    {{ Auth::user()->name }}

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,8 +35,8 @@
               
               <li class="dropdown dropdown-user nav-item">
               <a href="#" data-toggle="dropdown" class="dropdown-toggle nav-link dropdown-user-link">
-              <span class="avatar avatar-online"><img src="{{ asset('app-assets/images/portrait/small/avatar-s-1.png')}}" alt="avatar">
-              <i></i>              </span><span class="user-name"> @yield('user') </span></a>
+              <span class="avatar avatar-online"><img src="{{ asset('backoffice/app-assets/images/portrait/small/avatar-s-1.png')}}" alt="avatar">
+              <i></i>              </span><span class="user-name"> {{ Auth::user()->name }} </span></a>
                 <div class="dropdown-menu dropdown-menu-right">
                     <a href="#" class="dropdown-item"><i class="ft-user"></i> Edit Profile</a>
                    
