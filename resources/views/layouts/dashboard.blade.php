197d2bb535005503160a47f642bd1f73091b550a
asdrubal <jose_asdrubal1@hotmail.com>
2017-08-25 00:34:59 -0500

    footer fixed

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -156,12 +156,12 @@
 <!-- ////////////////////////////////////////////////////////////////////////////-->
 
 
-<footer class="footer footer-static footer-light navbar-shadow ">
+<footer class="footer footer-static footer-light navbar-shadow " style="position: fixed; height: 40px; bottom: 0; width: 100%;">
     <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2">
         <span class="float-md-left d-xs-block d-md-inline-block">Copyright  &copy; 2017
             <a href="#" target="_blank" class="text-bold-800 grey darken-2">Bitsyte </a>, All rights reserved.
         </span>
-        <span class="float-md-right d-xs-block d-md-inline-block hidden-md-down">... <i class="ft-heart pink"></i></span>
+        <span class="float-md-right d-xs-block d-md-inline-block hidden-md-down">... </span>
     </p>
 </footer>
 
