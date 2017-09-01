b1873ace638dc5a9e97c7548fe03a75828a3ee9a
asdrubal <jose_asdrubal1@hotmail.com>
2017-08-31 21:54:09 -0500

    add carousel paquete in new user

--- resources/views/Users/new.blade.php
+++ resources/views/Users/new.blade.php
@@ -48,6 +48,50 @@
                             <form class="form">
                                 <div class="form-body">
                                     <h4 class="form-section"><i class="ft-user"></i> New User</h4>
+
+                                    <div class="row">
+                                        <div class="col-md-offset-3 col-sm-offset-3 col-md-6 col-sm-12 ">
+                                            <div class="" style="margin: 0 30%">
+                                                <div class="card">
+                                                    <div class="card-header">
+                                                        <h4 class="card-title">Select a Package</h4>
+                                                    </div>
+                                                    <div class="card-body collapse in">
+                                                        <div class="card-block">
+                                                            <p>Whether the carousel should cycle continuously or have hard stops. Default: true</p>
+                                                            <div id="carousel-wrap" class="carousel slide" data-ride="carousel" data-wrap="false">
+                                                                <ol class="carousel-indicators">
+                                                                    <li data-target="#carousel-wrap" data-slide-to="0" class="active"></li>
+                                                                    <li data-target="#carousel-wrap" data-slide-to="1"></li>
+                                                                    <li data-target="#carousel-wrap" data-slide-to="2"></li>
+                                                                </ol>
+                                                                <div class="carousel-inner" role="listbox">
+                                                                    <div class="carousel-item active">
+                                                                        <img src="{{ asset('backoffice/app-assets/images/carousel/02.jpg') }}" alt="First slide">
+                                                                    </div>
+                                                                    <div class="carousel-item">
+                                                                        <img src="{{ asset('backoffice/app-assets/images/carousel/04.jpg') }}" alt="Second slide">
+                                                                    </div>
+                                                                    <div class="carousel-item">
+                                                                        <img src="{{ asset('backoffice/app-assets/images/carousel/08.jpg') }}" alt="Third slide">
+                                                                    </div>
+                                                                </div>
+                                                                <a class="left carousel-control" href="#carousel-wrap" role="button" data-slide="prev">
+                                                                    <span class="icon-prev" aria-hidden="true"></span>
+                                                                    <span class="sr-only">Previous</span>
+                                                                </a>
+                                                                <a class="right carousel-control" href="#carousel-wrap" role="button" data-slide="next">
+                                                                    <span class="icon-next" aria-hidden="true"></span>
+                                                                    <span class="sr-only">Next</span>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
                                     <div class="row">
                                         <div class="col-md-6">
                                             <div class="form-group">
