fcd4fdb49899cafb5ff03a35f029a88c9cbefbd4
asdrubal <jose_asdrubal1@hotmail.com>
2017-10-06 14:00:19 -0500

    se modifico layout de login y parciales

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,124 +1,33 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}" data-textdirection="ltr">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta name="description" content="BitSyte">
-    <meta name="keywords" content="layout">
-    <meta name="author" content="Bitsyte">
-    <title> Login </title>
+@extends ('layouts.login')
 
+@section('content')
+    <h6 class="card-subtitle line-on-side text-muted text-xs-center font-small-3 pt-2">
+        <span>We will send you a link to reset password.</span>
+    </h6>
+    <div class="card-body collapse in">
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+        <div class="card-block">
+            <form class="form-horizontal" action="{{route('password.email')}}" method="post" novalidate>
+                {{ csrf_field() }}
 
-    <link rel="apple-touch-icon" href="{{ asset('backoffice/app-assets/images/ico/apple-icon-120.png')}}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('backoffice/app-assets/images/ico/favicon.ico')}}">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i%7COpen+Sans:300,300i,400,400i,600,600i,700,700i"
-          rel="stylesheet">
-    <!-- BEGIN VENDOR CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/css/bootstrap.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/fonts/feather/style.css')}}">
-    <link rel="stylesheet" type="text/css"
-          href="{{ asset('backoffice/app-assets/fonts/font-awesome/css/font-awesome.css')}}">
-    <link rel="stylesheet" type="text/css"
-          href="{{ asset('backoffice/app-assets/fonts/flag-icon-css/css/flag-icon.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/vendors/css/extensions/pace.css')}}">
-    <link rel="stylesheet" type="text/css"
-          href="{{ asset('backoffice/app-assets/vendors/css/charts/jquery-jvectormap-2.0.3.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/vendors/css/charts/morris.css')}}">
-    <link rel="stylesheet" type="text/css"
-          href="{{ asset('backoffice/app-assets/vendors/css/extensions/unslider.css')}}">
-    <!-- END VENDOR CSS-->
-    <!-- BEGIN STACK CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/css/bootstrap-extended.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/css/app.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/css/colors.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/css/custom.css')}}">
-    <!-- END STACK CSS-->
-    <!-- BEGIN Page Level CSS-->
-    <link rel="stylesheet" type="text/css"
-          href="{{ asset('backoffice/app-assets/css/core/menu/menu-types/horizontal-menu.css')}}">
-    <link rel="stylesheet" type="text/css"
-          href="{{ asset('backoffice/app-assets/css/core/menu/menu-types/vertical-overlay-menu.css')}}">
+                @if(env('APP_LOGIN_WITH') == 'email')
+                    @include('auth.passwords.partial.email')
+                @else
+                    @include('auth.passwords.partial.username_email')
+                @endif
 
-    <!-- END Page Level CSS-->
-</head>
-
-<body data-open="click" data-menu="horizontal-menu" data-col="1-column"
-      class="horizontal-layout horizontal-menu 1-column  blank-page blank-page">
-<!-- //////////////////////////////////////-->
-<body data-open="click" data-menu="horizontal-menu" data-col="1-column" class="horizontal-layout horizontal-menu 1-column  blank-page blank-page">
-<!-- ////////////////////////////////////////////////////////////////////////////-->
-<div class="app-content container center-layout mt-2">
-    <div class="content-wrapper">
-        <div class="content-header row">
-        </div>
-        <div class="content-body"><section class="flexbox-container">
-                <div class="col-md-6 offset-md-3 col-xs-10 offset-xs-1 box-shadow-2 p-0">
-                    <div class="card border-grey border-lighten-3 px-2 py-2 m-0">
-                        <div class="card-header no-border pb-0">
-                            <div class="card-title text-xs-center">
-                                <img src="{{ asset('backoffice/images/bitsyte_logo.png') }}" alt="branding logo">
-                            </div>
-                            <h6 class="card-subtitle line-on-side text-muted text-xs-center font-small-3 pt-2"><span>We will send you a link to reset password.</span></h6>
-                        </div>
-                        <div class="card-body collapse in">
-                            @if (session('status'))
-                                <div class="alert alert-success">
-                                    {{ session('status') }}
-                                </div>
-                            @endif
-                            <div class="card-block">
-                                <form class="form-horizontal" action="{{route('password.email')}}" method="post" novalidate>
-                                    {{ csrf_field() }}
-
-                                    @if(env('APP_LOGIN_WITH') == 'email')
-                                        @include('auth.passwords.partial.email')
-                                    @else
-                                        @include('auth.passwords.partial.username_email')
-                                    @endif
-
-                                    <button type="submit" class="btn btn-outline-primary btn-lg btn-block"><i class="ft-unlock"></i> Recover Password</button>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="card-footer no-border">
-                            <p class="float-sm-left text-xs-center"><a href="{{ route('login') }}" class="card-link">Login</a></p>
-                            {{--<p class="float-sm-right text-xs-center">New to BitSite ? <a href="../register/register.html" class="card-link">Create Account</a></p>--}}
-                        </div>
-                    </div>
-                </div>
-            </section>
-
+                <button type="submit" class="btn btn-outline-primary btn-lg btn-block"><i class="ft-unlock"></i>
+                    Recover Password
+                </button>
+            </form>
         </div>
     </div>
-</div>
-<!-- ////////////////////////////////////////////////////////////////////////////-->
-
-
-<!-- BEGIN VENDOR JS-->
-<script src="{{ asset('backoffice/app-assets/vendors/js/vendors.js')}}" type="text/javascript"></script>
-<!-- BEGIN VENDOR JS-->
-<!-- BEGIN PAGE VENDOR JS-->
-<script type="text/javascript" src="{{ asset('backoffice/app-assets/vendors/js/ui/jquery.sticky.js')}}"></script>
-<script src="{{ asset('backoffice/app-assets/vendors/js/extensions/unslider-min.js')}}" type="text/javascript"></script>
-<link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/fonts/simple-line-icons/style.css')}}">
-<!-- END PAGE VENDOR JS-->
-<!-- BEGIN STACK JS-->
-<script src="{{ asset('backoffice/app-assets/js/core/app-menu.js')}}" type="text/javascript"></script>
-<script src="{{ asset('backoffice/app-assets/js/core/app.js')}}" type="text/javascript"></script>
-<!-- END STACK JS-->
-<!-- BEGIN PAGE LEVEL JS-->
-<!-- END PAGE LEVEL JS-->
-
-</body>
-</html>
-
-
-
-
-
-
-
-
-
-
+    <div class="card-footer no-border">
+        <p class="float-sm-left text-xs-center"><a href="{{ route('login') }}" class="card-link">Login</a></p>
+        {{--<p class="float-sm-right text-xs-center">New to BitSite ? <a href="../register/register.html" class="card-link">Create Account</a></p>--}}
+    </div>
+@endsection
---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}" data-textdirection="ltr">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="BitSyte">
+    <meta name="keywords" content="layout">
+    <meta name="author" content="Bitsyte">
+    <title> Login </title>
+
+
+    <link rel="apple-touch-icon" href="{{ asset('backoffice/app-assets/images/ico/apple-icon-120.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('backoffice/app-assets/images/ico/favicon.ico')}}">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i%7COpen+Sans:300,300i,400,400i,600,600i,700,700i"
+          rel="stylesheet">
+    <!-- BEGIN VENDOR CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/css/bootstrap.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/fonts/feather/style.css')}}">
+    <link rel="stylesheet" type="text/css"
+          href="{{ asset('backoffice/app-assets/fonts/font-awesome/css/font-awesome.css')}}">
+    <link rel="stylesheet" type="text/css"
+          href="{{ asset('backoffice/app-assets/fonts/flag-icon-css/css/flag-icon.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/vendors/css/extensions/pace.css')}}">
+    <link rel="stylesheet" type="text/css"
+          href="{{ asset('backoffice/app-assets/vendors/css/charts/jquery-jvectormap-2.0.3.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/vendors/css/charts/morris.css')}}">
+    <link rel="stylesheet" type="text/css"
+          href="{{ asset('backoffice/app-assets/vendors/css/extensions/unslider.css')}}">
+    <!-- END VENDOR CSS-->
+    <!-- BEGIN STACK CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/css/bootstrap-extended.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/css/app.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/css/colors.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/css/custom.css')}}">
+    <!-- END STACK CSS-->
+    <!-- BEGIN Page Level CSS-->
+    <link rel="stylesheet" type="text/css"
+          href="{{ asset('backoffice/app-assets/css/core/menu/menu-types/horizontal-menu.css')}}">
+    <link rel="stylesheet" type="text/css"
+          href="{{ asset('backoffice/app-assets/css/core/menu/menu-types/vertical-overlay-menu.css')}}">
+
+    <!-- END Page Level CSS-->
+</head>
+
+<body data-open="click" data-menu="horizontal-menu" data-col="1-column"
+      class="horizontal-layout horizontal-menu 1-column  blank-page blank-page">
+<!-- //////////////////////////////////////-->
+<div class="app-content container center-layout mt-2">
+    <div class="content-wrapper">
+        <div class="content-header row">
+        </div>
+        <div class="content-body">
+            <section class="flexbox-container">
+
+                <div class="col-md-6 offset-md-3 col-xs-10 offset-xs-1  box-shadow-2 p-0  ">
+                    <div class="card border-grey border-lighten-3 m-0">
+                        <div class="card-header no-border">
+                            <div class="card-title text-xs-center">
+                                <div class="p-1">
+                                    <img src="{{ asset('backoffice/images/bitsyte_logo.png') }}"
+                                         alt="branding logo"></div>
+                            </div>
+                        </div>
+
+                        @yield('content')
+
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</div>
+<!-- //////////////////////////////////////-->
+
+
+<!-- BEGIN VENDOR JS-->
+<script src="{{ asset('backoffice/app-assets/vendors/js/vendors.js')}}" type="text/javascript"></script>
+<!-- BEGIN VENDOR JS-->
+<!-- BEGIN PAGE VENDOR JS-->
+<script type="text/javascript" src="{{ asset('backoffice/app-assets/vendors/js/ui/jquery.sticky.js')}}"></script>
+<script src="{{ asset('backoffice/app-assets/vendors/js/extensions/unslider-min.js')}}" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="{{ asset('backoffice/app-assets/fonts/simple-line-icons/style.css')}}">
+<!-- END PAGE VENDOR JS-->
+<!-- BEGIN STACK JS-->
+<script src="{{ asset('backoffice/app-assets/js/core/app-menu.js')}}" type="text/javascript"></script>
+<script src="{{ asset('backoffice/app-assets/js/core/app.js')}}" type="text/javascript"></script>
+<!-- END STACK JS-->
+<!-- BEGIN PAGE LEVEL JS-->
+<!-- END PAGE LEVEL JS-->
+
+</body>
+</html>
+
+
+
+
+
+
