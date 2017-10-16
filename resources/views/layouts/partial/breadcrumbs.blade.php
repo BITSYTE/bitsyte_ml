abb852ca12d605f7b207a9973b21f536c04b7194
Pedro Luna <neko.hitokori@gmail.com>
2017-10-16 00:58:48 -0500

    fix breadcrums and other hotfix*

--- resources/views/layouts/partial/breadcrumbs.blade.php
+++ resources/views/layouts/partial/breadcrumbs.blade.php
@@ -9,7 +9,8 @@
                         @if( $i == count($breadcrumbs)-1 )
                             <li class="breadcrumb-item active">{{ $breadcrumbs['name'][$i] }}</li>
                         @else
-                            <li class="breadcrumb-item"><a href="/{{$breadcrumbs['route'][$i]}}">{{ $breadcrumbs['name'][$i] }}</a></li>
+                            <li class="breadcrumb-item"><a
+                                        href="/{{$breadcrumbs['route'][$i]}}">{{ $breadcrumbs['name'][$i] }}</a></li>
                         @endif
                     @endfor
                 </ol>
@@ -17,12 +18,13 @@
         </div>
     </div>
     <div id="bred-graph" class="content-header-right col-md-6 col-xs-12">
-        <div class="media width-250 float-xs-right">
-            <div class="media-left media-middle">
+        <div class="media width-300 float-xs-right">
+            <div class="media-right media-middle">
                 <div id="sp-bar-total-sales"></div>
             </div>
             <div class="media-body media-right text-xs-right">
-                <h3 class="m-0">${!! auth()->user()->wallets()->sum('balance') !!}</h3><span class="text-muted">Balance</span>
+                <h3 class="m-0">${!! number_format(auth()->user()->wallets()->sum('balance'),0) !!}</h3>
+                <span class="text-muted">Balance</span>
             </div>
         </div>
     </div>
