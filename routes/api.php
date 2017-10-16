93af4fe845539ba9d964985cc24fa6ebd854e031
asdrubal <jose_asdrubal1@hotmail.com>
2017-10-15 22:50:18 -0500

    binary tree ***

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('unilevel/children/{user}', 'Api\UnilevelTreeController@treeJson')->name('unilevel');
-Route::get('binary/children/{user}', 'Api\BinaryTreeController@treeJson')->name('binary');
+Route::any('binary/children/{user}', 'Api\BinaryTreeController@treeJson')->name('binary');
